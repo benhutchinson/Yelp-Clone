57337bb96eab766d9cb80b1bf46b91d3c52089f7
Claudia <claudia.donovan89@gmail.com>
2014-12-16 16:35:25 +0000

    some stuff

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,7 +13,7 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-  	params.require(:restaurant).permit(:name)
+  	params.require(:restaurant).permit(:name, :description)
   end
 
 end
