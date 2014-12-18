afe3d6b68595cf1421056ad479b984d77661f5a7
benhutchinson <ben.hutchinson.personal@gmail.com>
2014-12-18 17:20:52 +0000

    Fixed Restaurant / User Models To Reflect Image Asset

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ActiveRecord::Base
 
   has_many :reviews
   has_many :restaurants
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
